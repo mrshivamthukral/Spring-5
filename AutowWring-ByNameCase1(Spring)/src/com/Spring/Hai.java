36c15f3e8d4ecff1e54afe8984bb59f56553c831
mrshivamthukral <65995193+mrshivamthukral@users.noreply.github.com>
2020-08-04 21:10:56 +0530

    Add files via upload

---
+++ AutowWring-ByNameCase1(Spring)/src/com/Spring/Hai.java
@@ -0,0 +1,22 @@
+package com.Spring;
+
+public class Hai {
+
+		static {
+			System.out.println("Hai - S.B");
+		}
+	
+		public Hai() {
+			System.out.println("Hai - D.C");
+		}
+
+		@Override
+		public String toString() {
+			return "Hai Guys, I am Hai Bean";
+		}
+		
+		 
+
+	
+	
+}
