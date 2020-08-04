d0fd2c85fc7544f2d9cd76b8aeab46efe15c534b
mrshivamthukral <65995193+mrshivamthukral@users.noreply.github.com>
2020-08-04 21:12:43 +0530

    Add files via upload

---
+++ AutoWring-ByNameCase2(Spring)/src/com/Spring/AppConfig.java
@@ -0,0 +1,23 @@
+package com.Spring;
+
+import org.springframework.beans.factory.annotation.Autowire;
+
+import org.springframework.context.annotation.Bean;
+
+public class AppConfig {
+	
+	@Bean("hai")
+	 public Hai createHai() {
+		System.out.println("-------createHai() ------ called"); 
+	 Hai hai=new Hai();
+	 return hai;
+	 } 
+	
+	@Bean(name="myhello",autowire = Autowire.BY_NAME) 
+	public Hello createHello() {
+		System.out.println("-------createHello() ------ called"); 
+		Hello hello=new Hello();
+		return hello;
+	}
+	
+}
