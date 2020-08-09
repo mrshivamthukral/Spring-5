45f4f1641ba120e63611fa8c4e1252966bebb41b
mrshivamthukral <65995193+mrshivamthukral@users.noreply.github.com>
2020-08-09 21:09:04 +0530

    Add files via upload

---
+++ @Autowired(required=true)Case1(Spring)/src/com/Spring/Hello.java
@@ -0,0 +1,17 @@
+package com.Spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Hello {
+	
+	@Autowired
+	 Hai hai; 
+
+	 public void show() {
+	 System.out.println(hai);
+	 } 
+
+	
+	
+	
+}
