0696117d559b6eec6cec2bbba7074a1791f116fb
mrshivamthukral <65995193+mrshivamthukral@users.noreply.github.com>
2020-08-26 20:38:46 +0530

    Add files via upload

---
+++ @Autowired(required=false)Case3(Spring)/src/com/Spring/AppConfig.java
@@ -0,0 +1,19 @@
+package com.Spring;
+
+import org.springframework.context.annotation.Bean;
+
+public class AppConfig {
+	
+	@Bean(name="myhai1")
+	 public Hai createHai1() {
+	 Hai hai=new Hai();
+	 hai.setMsg("I am Hai Bean 1");
+	 return hai;
+	 } 
+	
+	@Bean(name="myhello")
+	 public Hello createHello() {
+	 return new Hello();
+	 } 
+	
+}
